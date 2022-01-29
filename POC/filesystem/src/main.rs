b955c5d3cb9d293b7332850a1ba1f53a5e211cb7
GlobalConqueror <89528213+GlobalConqueror@users.noreply.github.com>
2022-01-29 15:08:00 +0000

    Update main.rs

--- POC/filesystem/src/main.rs
+++ POC/filesystem/src/main.rs
@@ -2,15 +2,16 @@ use std::io;
 use std::io::*;
 #[allow(non_snake_case)]
 
+#[derive(Debug)]
 pub struct File {
-    name: String,
-    full_type: String,
+    name: String
 }
 
 fn main() {
     /*Variables*/
     let run: bool = true;
     let mut input = String::new();
+    let mut files = vec![File { name: "template.txt".parse().unwrap()}];
     /*On Start-Up*/
     println!("  DEMO SHELL");
     /*Main program*/
@@ -19,5 +20,15 @@ fn main() {
         io::stdout().flush().unwrap();
         stdin().read_line(&mut input).ok().expect("Error: Failed to read line");
         let userInstructCache: Vec<&str> = input.split(" ").collect();
+        if userInstructCache[0] == "touch" {
+            let newFile = File { name: userInstructCache[1].parse().unwrap(),};
+            files.push(newFile);
+            continue;
+        }
+        else if userInstructCache[0] == "ls" || userInstructCache[0] == "dir" {
+            for i in 0..files.len()  {
+                println!("{:#?}", files[i].name);
+            }
+        }
     }
 }
