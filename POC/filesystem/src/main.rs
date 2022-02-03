2ef83260647072b5db6174428a6518ce40b6d2ac
GlobalConqueror <89528213+GlobalConqueror@users.noreply.github.com>
2022-02-03 14:09:04 +0000

    Changed call system
    Adapted to component based model - this makes it easier to scale up, prototype in user land, and to adapt to kernel level operation

--- POC/filesystem/src/main.rs
+++ POC/filesystem/src/main.rs
@@ -1,56 +1,37 @@
 use std::io;
 use std::io::*;
+use std::env;
 
 #[derive(Debug)]
 pub struct File {
     name: String
 }
 
-fn trim(s: &mut String) {
-    if s.ends_with('\n') {
-        s.pop();
-        if s.ends_with('\r') {
-            s.pop();
-        }
-    }
-}
-
 #[allow(non_snake_case)]
 fn main() {
     /*Variables*/
     let run: bool = true;
-    let mut input = String::new();
     let mut files = vec![File { name: "template.txt".parse().unwrap()}];
-    let mut clear: Vec<&str> = vec![""];
     /*On Start-Up*/
     println!("  DEMO SHELL");
     /*Main program*/
-    while run {
-        print!("> ");
-        io::stdout().flush().unwrap();
-        stdin().read_line(&mut input).ok().expect("Error: Failed to read line");
-        trim(&mut input);
-        let mut userInstructCache: Vec<&str> = input.split(" ").collect();
-        for _i in 0..userInstructCache.len() {
-            println!("{:#?}", userInstructCache);
+    let mut args: Vec<String> = env::args().collect();
+    /*
+    - Implement some way of removing the exe call
+    - If done, change the if, else if statements to evaluate args[0] instead of args[1]
+    */
+        println!("{:#?}", args);
+    /*Input evaluation - Does not work*/
+    if args[1] == "touch" {
+        let newFile = File { name: args[1].parse().unwrap(),};
+        files.push(newFile);
+    }
+    else if args[1] == "ls" || args[0] == "dir" {
+        for i in 0..files.len()  {
+            println!("{:#?}", files[i].name);
         }
-        /*Input evaluation - Does not work*/
-        if userInstructCache[0] == "touch" {
-            let newFile = File { name: userInstructCache[1].parse().unwrap(),};
-            files.push(newFile);
-            assert_eq!(userInstructCache, clear);
-            continue;
-        }
-        else if userInstructCache[0] == "ls" || userInstructCache[0] == "dir" {
-            for i in 0..files.len()  {
-                println!("{:#?}", files[i].name);
-            }
-            assert_eq!(userInstructCache, clear);
-            continue;
-        }
-        else {
-            eprintln!("Invalid command");
-            assert_eq!(userInstructCache, clear);
-        }
+    }
+    else {
+        eprintln!("Invalid command");
     }
 }
